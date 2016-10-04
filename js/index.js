c832b5c907900647e419b8172d0fcbdef8ebc68c
HarrisonB <harrison.bachrach@gmail.com>
2016-10-03 17:48:08 -0700

    Big redesign

---
+++ js/index.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+	var count = $("#vertList li").length;
+	var i = 0;
+	var dist = -1 * $("#vertList li").outerHeight(true);
+	console.log(dist);
+	window.setInterval(function() {
+		if (i < count-1) {
+			$("#vertList").transition({y: `+=${dist}px`});
+			i += 1
+		} else {
+			$("#vertList").transition({y: "0"});
+			i = 0;
+		}
+	}, 5000);
+};
